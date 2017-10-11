8a0bd391ba1e874497de213bc7c49ba099a03dc0
David Ko <dk577@cornell.edu>
2017-10-11 16:53:18 -0400

    newgoogleauth

---
+++ secrets.js
@@ -0,0 +1,5 @@
+process.env.GOOGLE_CLIENT_ID = '285262277581-b6lroen8bp405q6tss5532av2qckvqb4.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'xOUYGOh9PnkniwZ_9cBh7HJa'
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
+process.env.DATABASE_URL = 'postgres://localhost:5432/jads-shopper'
+process.env.USER_URL_PATH_HISTORY = []
